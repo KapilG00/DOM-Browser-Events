1250beb74dd2c1b1a778cb05aa258c7da7dc9fac
Kapil Gupta <kapil.gupta4949@gmail.com>
2024-06-23 20:08:44 +0530

    demonstrated beforeUnload event

--- Events/Removing EventListeners/index.js
+++ Events/Removing EventListeners/index.js
@@ -14,18 +14,20 @@ headingElement.addEventListener("click", clickHandler); // abc@1234
 // Implement headingElement.removeEventListener("click", clickHandler);
 
 // Hacky way to remove just for demonstration.
-setTimeout(() => {
-  headingElement.removeEventListener("click", clickHandler); // abc@1234
-  //   headingElement.removeEventListener("mouseenter", mouseEnterHandler);
-}, 5000);
+// setTimeout(() => {
+//   headingElement.removeEventListener("click", clickHandler); // abc@1234
+//   //   headingElement.removeEventListener("mouseenter", mouseEnterHandler);
+// }, 5000);
 
 // NOTE: We need to give same function (Address wise) to both addEventListener and removeEventListener
 //       that why I have not taken the arrow function.
 
 // Real use of "beforeunload" event.
 
-document.addEventListener("beforeunload", () => {
+document.addEventListener("beforeunload", (event) => {
   // cleanup, ......
+  event.preventDefault();
   headingElement.removeEventListener("click", clickHandler); // abc@1234
+
   // headingElement.removeEventListener("mouseenter", mouseEnterHandler);
 });
